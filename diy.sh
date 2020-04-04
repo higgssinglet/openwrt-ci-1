830cb95a392b7d847d798a5680b3eec80e3b0daa
wei5i42 <wei5i42@qq.com>
2020-04-04 22:28:54 +0800

    update files: 20200404 22:28

--- diy.sh
+++ diy.sh
@@ -1,19 +1,9 @@
 #!/bin/bash
 cd /home/runner/openwrt
 
-echo 'Update luci-app-passwall & openwrt-chinadns-ng'
-rm -rf /home/runner/openwrt/package/lienol/luci-app-passwall/
-rm -rf /home/runner/openwrt/package/ctcgfw/openwrt-chinadns-ng/
-cp -R /home/runner/openwrt-package/lienol/luci-app-passwall/ /home/runner/openwrt/package/lienol/
-cp -R /home/runner/openwrt-package/package/chinadns-ng/ /home/runner/openwrt/package/lienol/
-
 echo 'Update luci-theme-argon'
 rm -rf /home/runner/openwrt/package/lean/luci-theme-argon/
 cp -R /home/runner/luci-theme-argon/ /home/runner/openwrt/package/lean/
-
-echo 'Add luci-app-vssr'
-rm -rf /home/runner/openwrt/package/ctcgfw/luci-app-vssr/
-cp -R /home/runner/luci-app-vssr/ /home/runner/openwrt/package/lean/
 
 echo 'Fix serverchan temperature bug'
 sed -i "s?/sys/class/thermal/thermal_zone\*/temp?/sys/class/hwmon/hwmon0/temp1_input?g" /home/runner/openwrt/package/ctcgfw/luci-app-serverchan/root/usr/bin/serverchan/serverchan
@@ -24,9 +14,9 @@ sed -i "s/luci-theme-bootstrap/luci-theme-argon/g" /home/runner/openwrt/feeds/lu
 echo 'Fix koolproxyR bug'
 sed -i "s/\$(ARCH),arm/\$(ARCH),aarch64/g" /home/runner/openwrt/package/ctcgfw/luci-app-koolproxyR/Makefile
 
-echo 'Install po2lmo'
-rm -rf /home/runner/openwrt/package/ctcgfw/luci-app-openclash
-git clone https://github.com/vernesong/OpenClash.git /home/runner/openwrt/package/ctcgfw/luci-app-openclash/
-pushd /home/runner/openwrt/package/ctcgfw/luci-app-openclash/luci-app-openclash/tools/po2lmo
-make && sudo make install
-popd
+#echo 'Install po2lmo'
+#rm -rf /home/runner/openwrt/package/ctcgfw/luci-app-openclash
+#git clone https://github.com/vernesong/OpenClash.git /home/runner/openwrt/package/ctcgfw/luci-app-openclash/
+#pushd /home/runner/openwrt/package/ctcgfw/luci-app-openclash/luci-app-openclash/tools/po2lmo
+#make && sudo make install
+#popd
